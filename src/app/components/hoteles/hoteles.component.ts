69e6706b1e45059acb0b9774ab32529d2783b530
LautaroDiharce <lautarodiharce@gmail.com>
2021-11-11 16:23:36 -0300

    1 file modified

--- src/app/components/hoteles/hoteles.component.ts
+++ src/app/components/hoteles/hoteles.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Hotel } from '../../models/hotel';
 import { HotelService } from '../../services/hotel.service';
 @Component({
   selector: 'app-hoteles',
@@ -12,27 +14,54 @@ export class HotelesComponent implements OnInit {
     B: '(Eliminar)',
     M: '(Modificar)',
     C: '(Consultar)',
-    L: '(Listado)'
+    L: '(Listado)',
   };
-  AccionABMC = 'L'; 
-  constructor() {}
+  AccionABMC = 'L';
+  FormBusqueda: FormGroup;
+  Hoteles: Hotel[] = null;
+  RegistrosTotal: number;
+  constructor(
+    private hotelService: HotelService,
+    public formBuilder: FormBuilder
+  ) {}
 
-  ngOnInit() {}
-  
-
+  ngOnInit() {
+    this.FormBusqueda = this.formBuilder.group({
+      Nombre: [''],
+      Activo: [null],
+    });
+    // this.FormRegistro = this.formBuilder.group({
+    //   IdArticulo: [0],
+    //   Nombre: [
+    //     '',
+    //     [Validators.required, Validators.minLength(4), Validators.maxLength(55)]
+    //   ],
+    //   Precio: [null, [Validators.required, Validators.pattern('[0-9]{1,7}')]],
+    //   Stock: [null, [Validators.required, Validators.pattern('[0-9]{1,7}')]],
+    //   CodigoDeBarra: [
+    //     '',
+    //     [Validators.required, Validators.pattern('[0-9]{13}')]
+    //   ],
+    //   IdArticuloFamilia: ['', [Validators.required]],
+    //   FechaAlta: [
+    //     '',
+    //     [
+    //       Validators.required,
+    //       Validators.pattern(
+    //         '(0[1-9]|[12][0-9]|3[01])[-/](0[1-9]|1[012])[-/](19|20)[0-9]{2}'
+    //       )
+    //     ]
+    //   ],
+    //   Activo: [true]
+    // });
+  }
 
   Buscar() {
-    this.HotelService
-      .get(
-        this.FormBusqueda.value.Nombre,
-        this.FormBusqueda.value.Activo,
-        this.Pagina
-      )
+    this.hotelService
+      .get(this.FormBusqueda.value.Nombre, this.FormBusqueda.value.Habilitado)
       .subscribe((res: any) => {
-        this.Items = res.Items;
+        this.Hoteles = res.Hoteles;
         this.RegistrosTotal = res.RegistrosTotal;
       });
   }
 }
-
-
